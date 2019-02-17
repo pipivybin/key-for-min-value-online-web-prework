fe9b8fd69d87148d42293cc172628a27e4cf5edc
Ming Duan <11203duan@gmail.com>
2019-02-17 06:53:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  num = 10000
+  name_hash.each do
+    |name, value| 
+    num = value if value < num
+  end
 end
